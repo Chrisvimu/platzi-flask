ca3d23fdf70c23127551e39423fca9adee8e3680
= <=>
2021-02-22 02:19:09 -0300

    added alert for logIn and now IP is hidden, using Flask

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRET'
 
 todos = ['Buy stuff', 'Send buy request', 'get product']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -24,12 +31,25 @@ def index():
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Username successfully registered!')
+
+        return redirect(url_for('index'))
+
     # two ** before a dictionary expend it automatically
     return render_template('hello.html', **context)

00ce27f5cfde0d9b05ce0cb0dd4ac356e01d35de
= <=>
2021-02-22 00:37:20 -0300

    Added automatization script and flask-bootstrap for development

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRET'
 
 todos = ['Buy stuff', 'Send buy request', 'get product']
 
@@ -14,14 +19,14 @@ def not_found(error):
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': todos,

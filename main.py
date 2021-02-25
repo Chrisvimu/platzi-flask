45dc67f24cd118728f89e153a75e60a3004d08e6
= <=>
2021-02-24 22:10:51 -0300

    Added factoring for HTML views

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+app = create_app()
 
 app.config['SECRET_KEY'] = 'SUPER SECRET'
 
 todos = ['Buy stuff', 'Send buy request', 'get product']
-
-
-class LoginForm(FlaskForm):
-    username = StringField('User Name', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Submit')
 
 
 @app.cli.command()
--- venv_auto.sh
+++ venv_auto.sh
@@ -4,5 +4,5 @@ pip install -r requirements.txt
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
 export FLASK_ENV=development
-
+export FLASK_TEST=test
 flask run

265fd573ca6df16a53a18038eabdd0da614eafad
= <=>
2021-02-26 22:02:43 -0300

    Added changes to autentification of credentials, for use of google dtabase cloud services and login modulus

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, session, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user
 from . import auth
 from app.forms import LoginForm
+from app.firestore_service import get_user
+from app.models import UserModel, UserData
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -11,9 +14,35 @@ def login():
     }
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('Username successfully registered!')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo')
+
+                redirect(url_for('hello'))
+            else:
+                flash('Not correct information, des not match')
+
+        else:
+            flash('The user does not exist')
 
         return redirect(url_for('index'))
     return render_template('login.html', **context)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Return Soon!')
+
+    return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import unittest
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-import unittest
+from flask_login import login_required, current_user
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
 
 app = create_app()
 
@@ -32,12 +34,13 @@ def index():
 
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
     context = {
         'user_ip': user_ip,
-        'todos': todos,
+        'todos': get_todos(user_id=username),
         'username': username
     }
 
